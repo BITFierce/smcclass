65da7a847f19dab96a705f5da38e9a63418dc10c
Yeehok <Yeehok@YeehokdeMacBook-Pro.local>
2016-03-15 13:34:53 +0800

    js+css修改
    css修改为自适应，js修改为可以缩合

--- 前端/js/Show.js
+++ 前端/js/Show.js
@@ -1,13 +1,26 @@
-var arrays = new Array("rule","sheet","inform"); 
-function Show(tagId) { 
-for (var i = 0; i < arrays.length; i++) { 
-if (arrays[i] == tagId) { 
-document.getElementById(arrays[i]).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
-document.getElementById(arrays[i]).style.display = "block"; 
-} 
-else { 
-document.getElementById(arrays[i]).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
-document.getElementById(arrays[i]).style.display = "none"; 
-} 
-} 
-} 
+var arrays = new Array("rule","sheet","inform");
+var lastTagId = -1;
+function Show(tagId)
+{
+    if (lastTagId == tagId)
+    {
+        lastTagId = -1;
+        document.getElementById(tagId).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
+        document.getElementById(tagId).style.display = "none";
+    }
+    else
+    {
+        for (var i = 0; i < arrays.length; i++)
+        { 
+            if (arrays[i] == tagId) { 
+                document.getElementById(arrays[i]).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
+                document.getElementById(arrays[i]).style.display = "block"; 
+            } 
+            else { 
+                document.getElementById(arrays[i]).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
+                document.getElementById(arrays[i]).style.display = "none"; 
+            } 
+        }
+        lastTagId = tagId;
+    }
+}
