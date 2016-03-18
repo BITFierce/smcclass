7c0188b5e0c35e763ba4541326721bd05204f922
osmanthus1874 <osmanthus1874@gmail.com>
2016-03-18 17:23:21 +0800

    首页样式调整

--- 前端/js/Show.js
+++ 前端/js/Show.js
@@ -1,26 +1,62 @@
-var arrays = new Array("rule","sheet","inform");
-var lastTagId = -1;
-function Show(tagId)
-{
-    if (lastTagId == tagId)
-    {
-        lastTagId = -1;
-        document.getElementById(tagId).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
-        document.getElementById(tagId).style.display = "none";
-    }
-    else
-    {
-        for (var i = 0; i < arrays.length; i++)
-        { 
-            if (arrays[i] == tagId) { 
-                document.getElementById(arrays[i]).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
-                document.getElementById(arrays[i]).style.display = "block"; 
-            } 
-            else { 
-                document.getElementById(arrays[i]).parentNode.style.backgroundColor = "RGB(0,0,0)"; 
-                document.getElementById(arrays[i]).style.display = "none"; 
-            } 
-        }
-        lastTagId = tagId;
-    }
+function change(a){
+	if(a==0){
+		$("#u0").slideToggle();
+		$('#l0').toggleClass('open');	
+		$("#u1").slideUp();
+		$('#l1').removeClass('open');
+		$("#u2").slideUp();
+		$('#l2').removeClass('open');
+		$("#u3").slideUp();
+		$('#l3').removeClass('open');
+		$("#u4").slideUp();
+		$('#l4').removeClass('open');
+	}
+	if(a==1){
+		$("#u1").slideToggle();
+		$('#l1').toggleClass('open');
+		$("#u0").slideUp();
+		$('#l0').removeClass('open');
+		$("#u2").slideUp();
+		$('#l2').removeClass('open');
+		$("#u3").slideUp();
+		$('#l3').removeClass('open');
+		$("#u4").slideUp();
+		$('#l4').removeClass('open');
+	}
+	if(a==2){
+		$("#u2").slideToggle();
+		$('#l2').toggleClass('open');
+		$("#u1").slideUp();
+		$('#l1').removeClass('open');
+		$("#u0").slideUp();
+		$('#l0').removeClass('open');
+		$("#u3").slideUp();
+		$('#l3').removeClass('open');
+		$("#u4").slideUp();
+		$('#l4').removeClass('open');
+	}
+	if(a==3){
+		$("#u3").slideToggle();
+		$('#l3').toggleClass('open');
+		$("#u1").slideUp();
+		$('#l1').removeClass('open');
+		$("#u2").slideUp();
+		$('#l2').removeClass('open');
+		$("#u0").slideUp();
+		$('#l0').removeClass('open');
+		$("#u4").slideUp();
+		$('#l4').removeClass('open');
+	}
+	if(a==4){
+		$("#u4").slideToggle();
+		$('#l4').toggleClass('open');
+		$("#u1").slideUp();
+		$('#l1').removeClass('open');
+		$("#u2").slideUp();
+		$('#l2').removeClass('open');
+		$("#u3").slideUp();
+		$('#l3').removeClass('open');
+		$("#u0").slideUp();
+		$('#l0').removeClass('open');
+	}
 }
