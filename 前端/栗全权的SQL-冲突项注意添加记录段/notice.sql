660fe1f36b9e7b9d3454e4136b9f1b78464d8c64
bigchestnut <lqq604350132@outlook.com>
2016-04-01 11:33:30 +0800

    测试版0.1.1 修正
    1.最新sql
    2.登陆页面背景图片

--- 前端/栗全权的SQL-冲突项注意添加记录段/notice.sql
+++ 前端/栗全权的SQL-冲突项注意添加记录段/notice.sql
@@ -1,23 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version phpStudy 2014
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- 主机: localhost
--- 生成日期: 2016 年 03 月 31 日 20:17
--- 服务器版本: 5.7.10-log
--- PHP 版本: 5.3.29
+-- Host: localhost
+-- Generation Time: 2016-04-01 04:24:40
+-- 服务器版本： 10.1.10-MariaDB
+-- PHP Version: 5.6.19
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- 数据库: `hrmdas`
+-- Database: `hrmdas`
 --
 
 -- --------------------------------------------------------
@@ -26,26 +26,39 @@ SET time_zone = "+00:00";
 -- 表的结构 `notice`
 --
 
-CREATE TABLE IF NOT EXISTS `notice` (
+CREATE TABLE `notice` (
+  `NoticeID` int(11) NOT NULL,
   `Author` varchar(20) NOT NULL,
-  `Time` date NOT NULL,
+  `Type` varchar(20) DEFAULT NULL,
+  `Time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Title` varchar(50) NOT NULL,
   `Text` varchar(2000) DEFAULT NULL,
   `spare1` varchar(20) DEFAULT NULL,
-  `spare2` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`Author`,`Time`)
+  `spare2` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 限制导出的表
+-- 转存表中的数据 `notice`
+--
+
+-- Indexes for dumped tables
 --
 
 --
--- 限制表 `notice`
+-- Indexes for table `notice`
 --
 ALTER TABLE `notice`
-  ADD CONSTRAINT `FK_Government` FOREIGN KEY (`Author`) REFERENCES `goverment` (`GovermentNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD PRIMARY KEY (`NoticeID`);
 
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `notice`
+--
+ALTER TABLE `notice`
+  MODIFY `NoticeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
