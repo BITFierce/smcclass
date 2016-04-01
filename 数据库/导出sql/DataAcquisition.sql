1d3f3193a7cbce1287019e3d555e6a8b16a10dbc
ZAZUIMINGYUE <1322184241@qq.com>
2016-04-01 16:37:53 +0800

    修改dataacquisition 属性 checklevel
    原属性拼写错误

--- 数据库/导出sql/DataAcquisition.sql
+++ 数据库/导出sql/DataAcquisition.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50710
 File Encoding         : 65001
 
-Date: 2016-04-01 15:40:16
+Date: 2016-04-01 16:35:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,7 +28,7 @@ CREATE TABLE `dataacquisition` (
   `FirstReason` varchar(200) DEFAULT NULL,
   `SecondReason` varchar(200) DEFAULT NULL,
   `ThirdReason` varchar(200) DEFAULT NULL,
-  `CheckLavel` int(11) NOT NULL COMMENT '0为 未提交 1 为提交 市未审核 2为 市审核通过 省未审核 3为省审核通过',
+  `CheckLevel` int(11) NOT NULL COMMENT '0为 未提交 1 为提交 市未审核 2为 市审核通过 省未审核 3为省审核通过',
   `CollectionTime` date NOT NULL,
   `spare1` varchar(20) DEFAULT NULL,
   `spare2` varchar(20) DEFAULT NULL,
