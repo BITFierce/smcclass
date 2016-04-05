2624add021b5cc63f887872173f64a425abedc47
Yeehok <Yeehok@YeehokdeMacBook-Pro.local>
2016-04-05 17:06:28 +0800

    发布正式版v1.2
    包括正式版内容和打包版本。

--- Version1.2/province/survey.php
+++ Version1.2/province/survey.php
@@ -93,11 +93,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 					<td><select name="SurveyPeriodID">
 					<?php 
 						//连接数据库
-						$connect=mysql_connect("localhost:3306","root","root")or die ("不能连接数据库");
-						//强转一下数据库字符
-						mysql_query("set names 'utf8'",$connect);
-						mysql_select_db("hrmdas",$connect)or die("选择数据库失败");
-						//获得调查期选项
+						include '../sql/sqlname.php';
+						$connect=mysql_connect($sql_host,$sql_user,$sql_pass) or die('Could not connect: ' . mysql_error());
+						mysql_select_db($sql_name, $connect);
 						mysql_query("set names 'utf8'",$connect);
 						$sql="select * from `surveyperiod`";
 						$result=mysql_query($sql,$connect);
